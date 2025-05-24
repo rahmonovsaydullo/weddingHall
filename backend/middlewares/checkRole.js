04794dc6d527eeba4a6c24260032a5731739d187
rahmonovsaydullo <saydullo779@gmail.com>
2025-05-24 20:43:52 +0500

    I created checkrole middleware

---
+++ backend/middlewares/checkRole.js
@@ -0,0 +1,18 @@
+const checkRole = (allowedRoles) => {
+    return (req, res, next) => {
+        try {
+            if(!req.user){
+                return res.status(403).json({message: "User not logged in"})
+            }
+            if(allowedRoles.includes(req.user.role)) { // teacher
+				next()
+			}
+			else {
+				return res.status(403).json({ message: "You don't have access to this action" });
+			}
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(error.message)
+        }
+    }
+}
